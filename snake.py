eca4abb736c2f750ca779eceee32de8295f107d9
Rushikesh Chavan <rushic6@gmail.com>
2022-07-22 21:11:14 +0530

    Added the snake movement functionalities to the code

--- snake.py
+++ snake.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 
 SNAKE_POSITION = [(0, 0), (-21, 0), (-41, 0)]
+FORWARD_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 
 class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
 
     def create_snake(self):
         for position in SNAKE_POSITION:
@@ -21,4 +27,20 @@ class Snake:
             new_x = self.snake[i - 1].xcor()
             new_y = self.snake[i - 1].ycor()
             self.snake[i].goto(new_x, new_y)
-        self.snake[0].forward(20)
+        self.head.forward(FORWARD_DISTANCE)
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
