eee01ef58e2bc55c9d6ee33374d6cdf3c03704f0
Rushikesh Chavan <rushic6@gmail.com>
2022-07-22 22:39:18 +0530

    Added all the game functionalities.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -21,5 +25,22 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move_forward()
+
+    # if snake collides with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.grow()
+        scoreboard.get_score()
+
+    # if snake collides with the walls
+    if snake.head.xcor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    # if snake collides with itself
+    for snake_body in snake.snake[1:]:
+        if snake_body.distance(snake.head) < 5:
+            is_game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
